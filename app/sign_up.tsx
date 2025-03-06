94bda34328b20386c7d81186e2bfdadd295bff7d
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2025-03-06 20:48:32 +0530

    chore: add sign_up screen

--- app/sign_up.tsx
+++ app/sign_up.tsx
@@ -1,12 +1,34 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import React from "react";
-
+import { defaultStyles } from "@/constants/Styles";
 const SignUp = () => {
+  const onSignup = async () => {};
+
   return (
-    <View>
-      <Text>SignUp</Text>
+    <View style={styles.container}>
+      <Text style={defaultStyles.header}>Sign Up</Text>
+      <Text style={defaultStyles.descriptionText}>
+        Enter your phone number. We will send you a confirmation code there.
+      </Text>
+
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    marginVertical: 40,
+    flexDirection: "row",
+  },
+  input: {},
+});
 
 export default SignUp;
